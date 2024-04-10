21f0b53836854f0b94e534edf648173bcb95b82c
Valerie <kekvalery@gmail.com>
2024-04-10 13:46:40 +0300

    refactor: MovieList styles, add mock for movies

--- src/components/MovieList/index.tsx
+++ src/components/MovieList/index.tsx
@@ -1,21 +1,11 @@
 import { List } from 'antd';
-import { useLoaderData } from "react-router-dom";
-import { getMovieWithFilters } from "../../api";
 import MovieItem from "../MovieItem";
 
-export function loader() {
-  return getMovieWithFilters({
-    selectFields: ["id", "year", "name", "poster"],
-  });
-}
-
-const MovieList = () => {
-  const movies = useLoaderData() as Awaited<ReturnType<typeof getMovieWithFilters>>;
-
+const MovieList = ({movies}: {movies: MovieWithFilters[]}) => {
   return (
     <List
-      grid={{ gutter: 16, xs: 1, sm: 2, md: 3, lg: 4, xl: 4, xxl: 6 }}
-      dataSource={movies ?? undefined}
+      grid={{ gutter: 16, xs: 1, sm: 2, md: 3, lg: 3, xl: 4, xxl: 6 }}
+      dataSource={movies}
       renderItem={movie => (
         <List.Item>
           <MovieItem {...movie} />
